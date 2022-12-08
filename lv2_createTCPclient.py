1897b94e913339caeecc335d50459c975a2a33a0
unknown <huachen.zhang.23@eigsi.fr>
2022-12-08 11:48:12 +0100

    add all

---
+++ lv2_createTCPclient.py
@@ -0,0 +1,21 @@
+import socket
+
+clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+host = '10.237.96.17'
+# host = socket.gethostname()
+
+port = 444
+
+clientsocket.connect((host,port)) # connect to a remote socket. The argument varies depending on the address family
+
+message = clientsocket.recv(1024)
+# socket.recv(bufsize): receive data from the socket. Returns bytes. 
+# bufsize: max amount of data to be receive at once
+
+clientsocket.close()
+
+print(message.decode('ascii'))
+
+
+
